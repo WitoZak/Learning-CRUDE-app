82202423ad47883d344e39f259bcd0491097b39b
Witold Zakrzewski <witoldzakrzewski@gmail.com>
2023-02-06 18:41:28 +0100

    ADDED MovieLibraryApp using CRUD rule

---
+++ src/main/java/pl/witoldzakrzewski/movielibrary/MovieLibraryApp.java
@@ -0,0 +1,13 @@
+package pl.witoldzakrzewski.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryApp {
+
+    public static void main(String[] args) {
+                SpringApplication.run(MovieLibraryApp.class, args);
+    }
+
+}
---
+++ src/test/java/pl/witoldzakrzewski/movielibrary/LearningCrudeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.witoldzakrzewski.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCrudeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
